9d6ce5db36f2ef91b1f3edde9ef3f6ce9782db40
Tommy Ho <themooingpig@gmail.com>
2013-09-07 15:46:56 -0700

    More deployment changes

--- Server/TestProject1/config/deploy.rb
+++ Server/TestProject1/config/deploy.rb
@@ -1,25 +1,51 @@
-set :application, "TestProject1"
-set :repository,  "set your repository location here"
+require "bundler/capistrano"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :bundle_flags, "--no-deployment --quiet"
+set :application, "testapplication1"
+set :user, "tommy"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+set :scm, :git
+set :repository, "git@github.com:themooingpig@gmail.com/testapplication1.git"
+set :branch, "master"
+set :use_sudo, true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+server "tidal1.cloudapp.net", :web, :app, :db, primary: true
+
+
+set :deploy_to, "/home/#{user}/apps/#{application}"
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+ssh_options[:port] = 22
+
+
+namespace :deploy do
+  desc "Fix permissions"
+  task :fix_permissions, :roles => [ :app, :db, :web ] do
+    run "chmod +x #{release_path}/config/unicorn_init.sh"
+  end
+
+
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "service unicorn_#{application} #{command}"
+    end
+  end
+
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    sudo "mkdir -p #{shared_path}/config"
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+
+  task :symlink_config, roles: :app do
+    # Add database config here
+  end
+  after "deploy:finalize_update", "deploy:fix_permissions"
+  after "deploy:finalize_update", "deploy:symlink_config"
+end
