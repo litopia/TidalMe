fb92f3a6798fa3592c068711efd7636e962fabab
Tommy Ho <themooingpig@gmail.com>
2013-09-07 15:31:17 -0700

    TestProject1 Checkin

---
+++ Server/TestProject1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestProject1::Application.config.secret_key_base = 'eb21b695d73fcdd4e84d4f892b55372a3b3c45ad37377e58bde8aa31629f8c72341f5f53f8ba2d803d09ad4cc3836ba8555e26a0a240bc383059aaf4719d4ce5'
