ecc77b7b6113c95bd0fd733f7d223c80e5fb1f8e
Tommy Ho <themooingpig@gmail.com>
2013-09-07 18:58:26 -0700

    Removing forwarding

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ server "tidal1.cloudapp.net", :web, :app, :db, primary: true
 
 set :deploy_to, "/home/#{user}/apps/#{application}"
 default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
+#ssh_options[:forward_agent] = true
 ssh_options[:port] = 22
 
 
