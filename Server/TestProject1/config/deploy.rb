c565f01659a3ba72630d246f686e51ea4c79ee15
Tommy Ho <themooingpig@gmail.com>
2013-09-07 17:09:02 -0700

    A few changes to make subdir deployments work

--- Server/TestProject1/config/deploy.rb
+++ Server/TestProject1/config/deploy.rb
@@ -9,6 +9,7 @@ set :scm, :git
 set :repository, "git@github.com:themooingpig/TidalMe.git"
 set :branch, "master"
 set :use_sudo, true
+set :deploy_subdir, "server/testapplication1"
 
 
 server "tidal1.cloudapp.net", :web, :app, :db, primary: true
@@ -16,7 +17,7 @@ server "tidal1.cloudapp.net", :web, :app, :db, primary: true
 
 set :deploy_to, "/home/#{user}/apps/#{application}"
 default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
+#ssh_options[:forward_agent] = true
 ssh_options[:port] = 22
 
 
--- Server/TestProject1/config/unicorn.rb
+++ Server/TestProject1/config/unicorn.rb
@@ -2,7 +2,7 @@ worker_processes 2
 timeout 30
 listen "/tmp/unicorn.blog.sock"
  
-root = "/home/tommy/apps/blog_app/current"
+root = "/home/tommy/apps/testapplication1/current"
  
 working_directory root
  
